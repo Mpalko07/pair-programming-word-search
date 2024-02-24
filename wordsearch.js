9b1b989ee4e53b0823a60d387cc03908a94a3a36
Monica Palko <m.a.palko07@gmail.com>
2024-02-24 16:49:44 -0500

    -a

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
+//Week 5 Pair Programming By @ChrisPytel and @Mpalko07 
+
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+  
+  //for handling horizontal words
+    for (const row of letters) {
+        if (row.join('').includes(word)) return true;
+        if (row.reverse().join('').includes(word)) return true;
     }
+
+  //for handling vertical words
+    for (let columnNum = 0; columnNum < letters[0].length; columnNum++) {
+      let verticalWord = "";
+
+      for (let rowNum = 0; rowNum < letters.length; rowNum++) {
+        verticalWord += letters[rowNum][columnNum];
+      }
+      console.log(`verticalWord is `, verticalWord);
+      if (verticalWord.includes(word)) return true;
+      if (verticalWord.split("").reverse().join("").includes(word)) return true;
+    }
+  //otherwise as a catch all, returns false
+    return false;
 }
 
-module.exports = wordSearch
+
+
+module.exports = wordSearch;
